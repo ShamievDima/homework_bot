aed0dcb1a8024e9131608e3b48e7411469d53d1c
ShamievDima <90345143+ShamievDima@users.noreply.github.com>
2021-12-08 00:12:26 +0500

    Второе ревью

--- homework.py
+++ homework.py
@@ -7,6 +7,7 @@ import requests
 from requests import RequestException
 from dotenv import load_dotenv
 from telegram import Bot
+from http import HTTPStatus
 
 from logging.handlers import RotatingFileHandler
 
@@ -42,24 +43,33 @@ HOMEWORK_STATUSES = {
 
 def send_message(bot, message):
     """Отправляет сообщение в Телеграм."""
-    bot.send_message(TELEGRAM_CHAT_ID, message)
-    logger.info('Сообщение отправлено')
+    try:
+        bot.send_message(TELEGRAM_CHAT_ID, message)
+        logger.info('Сообщение отправлено')
+    except Exception as error:
+        logging.error(f'Сообщение не отправлено {error}')
 
 
 def get_api_answer(current_timestamp):
     """Возвращаем ответ API, преобразовав его к типам данных Python."""
     timestamp = current_timestamp
     params = {'from_date': timestamp}
-    homework_statuses = requests.get(
-        ENDPOINT,
-        headers=HEADERS,
-        params=params
-    )
-    if homework_statuses.status_code != 200:
+    try:
+        homework_statuses = requests.get(
+            ENDPOINT,
+            headers=HEADERS,
+            params=params
+        )
+    except ValueError:
+        logging.error('ENDPOINT недоступен')
+    if homework_statuses.status_code != HTTPStatus.OK:
         error_message = 'Ошибка Request'
         logging.error(error_message)
         raise RequestException(error_message)
-    response = homework_statuses.json()
+    try:
+        response = homework_statuses.json()
+    except Exception as error:
+        logging.error(f'Нет ожидаемого ответа сервера {error}')
     return response
 
 
@@ -68,15 +78,19 @@ def check_response(response):
     Проверяет ответ API на корректность.
     В случае успеха, выводит список домашних работ.
     """
-    homeworks = response['homeworks']
-    if len(homeworks) == 0:
-        error_message = 'Пустой список домашних работ'
-        logging.error(error_message)
-        raise ValueError(error_message)
-    elif not isinstance(response, dict):
+    if not isinstance(response, dict):
         error_message = 'Не верный тип ответа API'
         logging.error(error_message)
         raise TypeError(error_message)
+    if 'homeworks' not in response:
+        error_message = 'Ключ homeworks отсутствует'
+        logging.error(error_message)
+        raise KeyError(error_message)
+    if len(response) == 0:
+        error_message = 'Пустой список домашних работ'
+        logging.error(error_message)
+        raise ValueError(error_message)
+    homeworks = response.get('homeworks')
     return homeworks[0]
 
 
@@ -104,16 +118,22 @@ def parse_status(homework):
 
 def check_tokens():
     """Проверяет доступность переменных окружения."""
-    return False if (not PRACTICUM_TOKEN
-                    or not TELEGRAM_TOKEN
-                    or not TELEGRAM_CHAT_ID) else True
+    return not (
+        not PRACTICUM_TOKEN
+        or not TELEGRAM_TOKEN
+        or not TELEGRAM_CHAT_ID
+    )
 
 
 def main():
     """Основная логика работы бота."""
-    check_tokens()
+    print(check_tokens())
+    if not check_tokens():
+        error_message = 'Токены недоступны'
+        logging.error(error_message)
+        raise Exception(error_message)
     bot = Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = 0
+    current_timestamp = 0  # int(time.time())
 
     while True:
         try:
